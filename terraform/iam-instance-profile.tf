3a7937fbc8dd86f6ec1c2c90a870d5ac3314148d
tanya-domi <sheytanyadominic@gmail.com>
2025-05-03 04:04:56 +0200

    Update iam-instance-profile.tf

--- terraform/iam-instance-profile.tf
+++ terraform/iam-instance-profile.tf
@@ -1,4 +1,4 @@
 resource "aws_iam_instance_profile" "instance-profile" {
-  name = "Jumphost-instance-profile-v1.0"
+  name = "Jumphost-instance-profile-v1.1"
   role = aws_iam_role.iam-role.name
 }
